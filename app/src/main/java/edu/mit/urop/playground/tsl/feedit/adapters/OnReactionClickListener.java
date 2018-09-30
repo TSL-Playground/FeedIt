6f5d443b87583117458eb16c164629552f72280a
ogokdemir <ozan.gokdemir15@ncf.edu>
2018-09-30 15:54:31 -0400

    Added: Design Documentation, Note for Future Developers, Game Instruction, Installable .apk file.

--- app/src/main/java/edu/mit/urop/playground/tsl/feedit/adapters/OnReactionClickListener.java
+++ app/src/main/java/edu/mit/urop/playground/tsl/feedit/adapters/OnReactionClickListener.java
@@ -1,5 +1,11 @@
 package edu.mit.urop.playground.tsl.feedit.adapters;
 
+
+/**
+ * This is the interface through which the ReactionAdapter communicates the user taps
+ * on the reaction cards with the ViewReactionsActivity.
+ *
+ */
 public interface OnReactionClickListener {
 
     void onReactionCardTapped(int reactionIdx);
--- app/src/main/java/edu/mit/urop/playground/tsl/feedit/adapters/OnReviewClickListener.java
+++ app/src/main/java/edu/mit/urop/playground/tsl/feedit/adapters/OnReviewClickListener.java
@@ -1,5 +1,11 @@
 package edu.mit.urop.playground.tsl.feedit.adapters;
 
+
+/**
+ * This is the interface through which the ReviewAdapter communicates the review taps with the DisplayReviewsActivity.
+ *
+ */
+
 public interface OnReviewClickListener {
 
     void onShareButtonClicked(int reviewIdx);
